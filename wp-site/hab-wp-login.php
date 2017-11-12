8f982ca72a7094c3318a69609989397d7c0e9c24
Haseeb Ahmad Basil <haseeb.basil@gmail.com>
2017-11-13 00:02:08 +0500

    Allow hash based authentication

--- wp-site/hab-wp-login.php
+++ wp-site/hab-wp-login.php
@@ -1,17 +1,39 @@
-<?php
-
-include 'wp-includes/class-phpass.php';
-include 'wp-includes/wp-db.php';
-include 'wp-config.php';
-include 'wp-load.php';
-
-if(isset($_POST['user']) && isset($_POST['pass'])){
-	$user = htmlspecialchars($_POST['user'],ENT_QUOTES);
-	$pass = $_POST['pass'];
-
-	$userinfo = get_userdatabylogin($user);
-	$wp_hasher = new PasswordHash(8, TRUE);
-	$check = $wp_hasher->CheckPassword($pass, $userinfo->user_pass);	
+<?php
+
+
+
+include 'wp-includes/class-phpass.php';
+
+include 'wp-includes/wp-db.php';
+
+include 'wp-config.php';
+
+include 'wp-load.php';
+
+
+
+if(isset($_POST['user']) && isset($_POST['pass']))
+{
+
+	$user = htmlspecialchars($_POST['user'],ENT_QUOTES);
+
+	$pass = $_POST['pass'];
+
+	$userinfo = get_userdatabylogin($user);
+
+	if ($_POST['hash'] == 1)
+	{
+		if($pass == $userinfo->user_pass)
+		{
+			exit(json_encode($userinfo));
+		}
+		die(json_encode(array("ID"=>"null")));
+	}
+
+	$wp_hasher = new PasswordHash(8, TRUE);
+
+	$check = $wp_hasher->CheckPassword($pass, $userinfo->user_pass);	
+
 	if ($check)
 	{
 		exit(json_encode($userinfo));
@@ -20,7 +42,8 @@ if(isset($_POST['user']) && isset($_POST['pass'])){
 	{
 		die(json_encode(array("ID"=>"failled")));
 	}
-}else
+}
+else
 {
 	die(json_encode(array("ID"=>"null")));
 }
